469e91b6bb9c395c5981efb8e462ad6d38190d46
Marvin Baral <mba@jcbachmann.de>
2016-05-06 22:02:13 +0200

    initial commit in this repo. Current status like OpenCV-CameraAnalysis + first tries of communication with Arduino over serial port

---
+++ Qt/CameraControlledShooting-OpenCV-ArduinoSerial/CameraControlledShooting-OpenCV-ArduinoSerial.pro
@@ -0,0 +1,18 @@
+QT += core serialport
+QT -= gui
+
+CONFIG += c++11
+
+TARGET = CameraControlledShooting-OpenCV-ArduinoSerial
+CONFIG += console #cpp11
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
+
+#CONFIG -= qt
+INCLUDEPATH += /usr/local/include/opencv
+LIBS += -L/usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_video -lopencv_videoio -lopencv_videostab
+#SOURCES += main.cpp \
+ #   ownfunctions.cpp
---
+++ Qt/CameraControlledShooting-OpenCV-ArduinoSerial/main.cpp
@@ -0,0 +1,108 @@
+//#include <QCoreApplication>
+//int main(int argc, char *argv[])
+//{
+//    QCoreApplication a(argc, argv);
+//    return a.exec();
+//    }
+
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "ownfunctions.cpp"
+#include <QtSerialPort/QSerialPort>
+
+//http://rodrigoberriel.com/2014/11/using-opencv-3-qt-creator-3-2-qt-5-3/
+
+//=================================
+//============= BGR ===============
+//=================================
+//
+
+
+
+int detectBall(cv::Mat frame, int maxSize) {
+
+}
+
+int main(int argc, char* argv[]) {
+    cv::VideoCapture cap(0); // open the video camera no. 0
+    cv::Mat frame;
+    int baseColor[3];
+    if (!cap.isOpened()) {
+        std::cout << "Cannot open the video cam" << std::endl;
+        return -1;
+    }
+//    printParams(cap);
+    cv::namedWindow("MyVideo", CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+    std::cout << " BGR color model!!!" << std::endl;
+
+
+    QSerialPort serial; //https://www.youtube.com/watch?v=UD78xyKbrfk
+    serial.setPortName("/dev/ttyACM0");
+    serial.setBaudRate(QSerialPort::Baud9600);
+    serial.setDataBits(QSerialPort::Data8);
+    serial.setParity(QSerialPort::NoParity);
+    serial.setStopBits(QSerialPort::OneStop);
+    serial.setFlowControl(QSerialPort::NoFlowControl);
+    serial.open(QIODevice::ReadWrite);
+
+    serial.write("0;700;");
+//    QByteArray* response;
+
+    #define PULSE_LENGTH 20000 //microseconds
+    #define PULSE_SERVO_STANDARD 1500
+    #define PROTOCOL_USAGE "<char select {0-2}>;<int pulseTime {500-2500}>"
+    int pulseTimes[3] = {PULSE_SERVO_STANDARD, PULSE_SERVO_STANDARD, PULSE_SERVO_STANDARD}; //in ms
+
+    int keyPressed;
+    do {
+        cap.read(frame); //http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html#videocapture-read
+//        showAvgBGR(frame);
+//        showOnCMD(frame);
+
+        imshow("MyVideo", frame); //show the frame in "MyVideo" window
+
+        keyPressed = cv::waitKey(1);
+        switch (keyPressed) {
+            case -1: break;
+            case 107: //k
+                //give color of most centered pixel
+                for (int color = 0; color < 3; color++){
+                    baseColor[color] = *(frame.data + frame.step[0] * (frame.rows / 2) + frame.step[1] * (frame.cols / 2) + color);
+                    std::cout << baseColor[color] << "|";
+                }
+                std::cout << std::endl << "y: " << frame.rows / 2 << " x: " << frame.cols / 2 << std::endl;
+                break;
+            case 65362: //up
+                pulseTimes[0] += 100;
+                if (pulseTimes[0] > 2500) {
+                    pulseTimes[0] = 2500;
+                } else {
+                    serial.write("0;2100;");
+                    std::cout << "set pulse of Servo 0 to" << pulseTimes[0];
+                }
+                break;
+            case 65364: //down
+                pulseTimes[0] -= 100;
+                if (pulseTimes[0] < 500) {
+                    pulseTimes[0] = 500;
+                } else {
+                    serial.write("0;" + QString(pulseTimes[0]).toLocal8Bit() + ";");
+                    std::cout << "set pulse of Servo 0 to" << pulseTimes[0];
+                }
+                break;
+            default:
+                std::cout << "pressed " << keyPressed << std::endl;
+                break;
+        }
+
+        QByteArray response = serial.read(100);
+        std::cout << response.toStdString();
+    } while (keyPressed != 27);
+
+    std::cout << "esc key pressed - aborted" << std::endl;
+    serial.close();
+
+    return 0;
+}
+
