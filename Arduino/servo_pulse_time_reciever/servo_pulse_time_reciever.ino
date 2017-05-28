1ceea8fe47ea8f22d0860e0fa7137ba79bbe67c3
Marvin Baral <mba@jcbachmann.de>
2017-05-28 21:04:10 +0200

    updated shooting servo positions

--- Arduino/servo_pulse_time_reciever/servo_pulse_time_reciever.ino
+++ Arduino/servo_pulse_time_reciever/servo_pulse_time_reciever.ino
@@ -4,8 +4,8 @@ const unsigned short int PULSE_LENGTH = 20000; //microseconds
 const unsigned short int MIN_PULSE = 500;
 const unsigned short int MAX_PULSE = 1810;
 const unsigned short int REPEATIONS_OF_NEW_PULSE_TIME = 10;
-const unsigned short int UPTIME_SHOOTING_POS = 1300;
-const unsigned short int UPTIME_SHOOT_LOCK = 1050;
+const unsigned short int UPTIME_SHOOTING_POS = 1400;
+const unsigned short int UPTIME_SHOOT_LOCK = 1150;
 const unsigned short int INITIAL_PULSE_TIMES[3] = {1150, 1540, UPTIME_SHOOT_LOCK};
 const float TIME_PER_DEGREE = 10.69; //for both
 
